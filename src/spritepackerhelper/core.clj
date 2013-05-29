af8d3e83f69b7430c31f0b84f4c35e29f0b9deae
thomas <thomas@tower>
2013-05-29 17:45:04 +0200

    add gui feedback

--- src/spritepackerhelper/core.clj
+++ src/spritepackerhelper/core.clj
@@ -6,16 +6,23 @@
            (filedrop FileDrop FileDrop$Listener )
            (com.badlogic.gdx.tools.imagepacker TexturePacker2)))
 
+(def label-text "Drag the folder with sprites to pack here" )
+
 (defn App []
   (let [frame (JFrame. "Sprite Packer")
-        label (JLabel. "Drag the folder with sprites to pack here" JLabel/CENTER)
+        label (JLabel. label-text JLabel/CENTER)
         filedrop (FileDrop. label
                             (proxy [FileDrop$Listener] []
                               (filesDropped [files]
                                 (doseq [f files]
                                   (when (.isDirectory f)
-                                    (println "Processing directory " (.getName f) (.getPath f))
-                                    (TexturePacker2/main (into-array ^String [(.getPath f)])))))))]
+                                    (future 
+                                      (.setText label (str "Generate " (.getPath f) "-packed"))
+                                      (.repaint label)
+                                      (TexturePacker2/main (into-array ^String [(.getPath f)]))
+                                      (Thread/sleep 1000)
+                                      (.setText label label-text)
+                                      (.repaint label)))))))]
     (.setPreferredSize label (Dimension. 500 500))
     (.add (.getContentPane frame) label BorderLayout/CENTER)
     (doto frame
